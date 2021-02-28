6966e7bbda5131b2c543ffa97802b4602120457d
Hsinheng Hsu <hsin@Hsinhengs-MBP.fios-router.home>
2021-02-28 08:11:46 -0500

    update from github desktop
    test

--- vaccination_source.py
+++ vaccination_source.py
@@ -1,3 +1,4 @@
+"""Test"""
 from bs4 import BeautifulSoup
 import requests
 
@@ -13,20 +14,20 @@ def save_download_file(content, file_name):
         f.write(content)
     print("Done!")
 
-    
+
 def get_data_source_from_lws(file_index, save=False, save_file_name="file.csv"):
     """Get file source from learnwithshin."""
     url = "https://learnwithshin.github.io/docs/files/"
     content = get_content(url)
     soup = BeautifulSoup(content, "html.parser")
-    article = soup.find('article')
+    article = soup.find("article")
     anchors = article.find_all("a")
-    
+
     base_url = "https://learnwithshin.github.io/docs/"
     target_anchor = anchors[file_index]
     file_url = target_anchor["href"]
     file_url = file_url.replace("../", base_url)
-    
+
     res = requests.get(file_url)
     if save:
         save_download_file(res.content, save_file_name)
@@ -36,5 +37,3 @@ def get_data_source_from_lws(file_index, save=False, save_file_name="file.csv"):
 def content_to_df(bytes_content):
     file_obj = io.BytesIO(bytes_content)
     return pd.read_csv(file_obj)
-    
-
